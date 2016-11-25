62cbed66fea005c37976c83f0d7767c27c3d2c27
Carlos Eduado Angeline <edu_fecap2hotmail.com>
2016-11-25 03:32:28 -0200

    ajuste depois da reuiniao 25/11
    teve varios ajustes para ficar mais parecida com a psd possivel.

---
+++ html/js/ativo.js
@@ -0,0 +1,8 @@
+
+$( document ).ready(function() {
+   $( ".caixa1" ).click(function(e) {
+   $('.caixa1').removeClass("ativo-background");
+   $(this).addClass("ativo-background");
+  
+ });
+   });
